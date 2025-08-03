61734a7ee9befb2f0dd5541c0b9a14d80dd58a1e
emhgit <eharper0815@gmail.com>
2025-08-03 12:18:54 -0500

    add context providers to layout in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,36 @@
 import "../styles/main.css";
 import type { AppProps } from "next/app";
+import GlobalErrorBoundary from "../context/GlobalErrorBoundary";
+import { FirebaseProvider } from "../context/FirebaseContext";
+import { UserDataProvider } from "../context/UserDataContext/UserDataContext";
+import { DarkModeProvider } from "../context/DarkModeProvider";
+import { SignInProvider } from "../context/SignInContext";
+import { UserGroupsProvider } from "../hooks/groups/useUserGroups";
+import { EditorContext } from "../context/EditorContext";
+import { Toaster } from "react-hot-toast";
+import { BlindModeProvider } from "../context/BlindModeContext";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+  <>
+    <GlobalErrorBoundary>
+      <FirebaseProvider>
+        <UserDataProvider>
+          <DarkModeProvider>
+            <SignInProvider>
+              <UserGroupsProvider>
+                <BlindModeProvider>
+                  <EditorContext.Provider value={{ inEditor: false }}>
+                    <Component {...pageProps} />
+                  </EditorContext.Provider>
+                </BlindModeProvider>
+              </UserGroupsProvider>
+            </SignInProvider>
+          </DarkModeProvider>
+        </UserDataProvider>
+      </FirebaseProvider>
+    </GlobalErrorBoundary>
+    <Toaster position="top-right" />
+  </>
+  );
 }
