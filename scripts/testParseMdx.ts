2728d993876891f79a1c1e5daf746bea7fa5bfb5
emhgit <eharper0815@gmail.com>
2025-08-02 23:01:21 -0500

    create script to test parseMdxFile

---
+++ scripts/testParseMdx.ts
@@ -0,0 +1,25 @@
+import path from 'path';
+import { parseMdxFile } from '../src/lib/parseMdxFile';
+
+// set `module: commonjs` and `moduleResolution: Node` in in `tsconfig.json`
+async function test() {
+    try {
+        console.log('Starting to load content...');
+        const startTime = Date.now();
+        const filePath = path.join(process.cwd(), 'solutions/advanced/Ligatures.mdx');
+        const content = await parseMdxFile(filePath);
+        const endTime = Date.now();
+
+        console.log(`Took ${(endTime - startTime) / 1000} seconds`);
+        console.log(JSON.stringify(content, null, 2));
+    } catch (error) {
+        console.error('Error loading content:');
+        console.error(error);
+    } finally {
+        // Ensure the process exits, but give time for logs to flush
+        setTimeout(() => process.exit(0), 100);
+    }
+}
+
+// Run the test
+test();
