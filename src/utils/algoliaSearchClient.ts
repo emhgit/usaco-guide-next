d8707d7692f430da22fe9b8d5b01abd933d0288a
emhgit <eharper0815@gmail.com>
2025-08-07 14:44:24 -0500

    use liteClient instead of main client in src\utils\algoliaSearchClient.ts

--- src/utils/algoliaSearchClient.ts
+++ src/utils/algoliaSearchClient.ts
@@ -1,6 +1,6 @@
-import { algoliasearch } from "algoliasearch";
+import { liteClient } from "algoliasearch/lite";
 
-export const searchClient = algoliasearch(
+export const searchClient = liteClient(
   "3CFULMFIDW",
   "cdfadff49930ec4dc3327807edd10b8c"
 );
