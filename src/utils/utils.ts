252258ae95b0e187f13e85b9a06d3f5f35d4f496
emhgit <eharper0815@gmail.com>
2025-08-07 13:52:27 -0500

    uncomment out getModulesForDivision in src\utils\utils.ts

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,18 +1,18 @@
-import MODULE_ORDERING from '../../content/ordering';
-import { ModuleInfo } from '../models/module';
-import { MdxContent } from '../types/content';
+import MODULE_ORDERING from "../../content/ordering";
+import { ModuleInfo } from "../models/module";
+import { MdxContent } from "../types/content";
 
-/*export const getModulesForDivision = (
+export const getModulesForDivision = (
   allModules: {
-    [key: string]: Queries.SyllabusQuery['modules']['nodes'][0];
+    [key: string]: MdxContent;
   },
   division: keyof typeof MODULE_ORDERING
 ) => {
-  return MODULE_ORDERING[division].map(k => ({
+  return MODULE_ORDERING[division].map((k) => ({
     name: k.name,
-    items: k.items.map(k2 => {
+    items: k.items.map((k2) => {
       if (!allModules.hasOwnProperty(k2)) {
-        throw 'Module not found: ' + k2;
+        throw "Module not found: " + k2;
       }
       return {
         ...allModules[k2 as string],
@@ -21,7 +21,7 @@ import { MdxContent } from '../types/content';
     }),
     description: k.description,
   }));
-};*/
+};
 
 export function graphqlToModuleInfo(mdx: MdxContent): ModuleInfo {
   return new ModuleInfo(
@@ -42,5 +42,5 @@ export function graphqlToModuleInfo(mdx: MdxContent): ModuleInfo {
 
 // https://stackoverflow.com/questions/2218999/how-to-remove-all-duplicates-from-an-array-of-objects
 export function removeDuplicates<T>(arr: T[]): T[] {
-  return [...new Map(arr.map(item => [JSON.stringify(item), item])).values()];
+  return [...new Map(arr.map((item) => [JSON.stringify(item), item])).values()];
 }
