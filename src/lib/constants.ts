3fafda871d3bda1fba0ff3d3f6fac0d387b69cda
emhgit <eharper0815@gmail.com>
2025-08-11 07:08:57 -0500

    add public/data to yarn clean script and create constants.ts file for filepath constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,12 @@
+import path from "path";
+
+export const DATA_DIR = path.join(process.cwd(), "public", "data");
+export const SLUG_ID_MAPPING_FILE = path.join(DATA_DIR, "slug-id-mapping.json");
+export const CACHED_MODULES_FILE = path.join(DATA_DIR, "cached-modules.json");
+export const CACHED_SOLUTIONS_FILE = path.join(DATA_DIR, "cached-solutions.json");
+export const CACHED_PROBLEMS_FILE = path.join(DATA_DIR, "cached-problems.json");
+export const CACHED_MODULE_PROBLEM_LISTS_FILE = path.join(DATA_DIR, "cached-module-problem-lists.json");
+export const CACHED_COW_IMAGES_FILE = path.join(DATA_DIR, "cached-cow-images.json");
+export const CACHED_MODULE_FRONTMATTER_FILE = path.join(DATA_DIR, "cached-module-frontmatter.json");
+export const CACHED_SOLUTION_FRONTMATTER_FILE = path.join(DATA_DIR, "cached-solution-frontmatter.json");
+export const CACHED_IMAGES_FILE = path.join(DATA_DIR, "cached-images.json");
