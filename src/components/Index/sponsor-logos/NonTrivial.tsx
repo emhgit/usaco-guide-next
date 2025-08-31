8d28dd7e4ccf97b1136e87a2e874015790873899
emhgit <eharper0815@gmail.com>
2025-08-31 15:32:08 -0500

    change image sizes for nontrivial and easyfuncoding to be 1500x500

--- src/components/Index/sponsor-logos/NonTrivial.tsx
+++ src/components/Index/sponsor-logos/NonTrivial.tsx
@@ -11,8 +11,8 @@ const NonTrivial = () => {
           blurDataURL={`data:image/svg+xml;base64,${Buffer.from(
             `<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1 1'/>`
           ).toString("base64")}`}
-          height={48}
-          width={48}
+          height={1500}
+          width={500}
         />
       </div>
       <div className="dark:hidden">
@@ -23,8 +23,8 @@ const NonTrivial = () => {
           blurDataURL={`data:image/svg+xml;base64,${Buffer.from(
             `<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 1 1'/>`
           ).toString("base64")}`}
-          height={48}
-          width={48}
+          height={1500}
+          width={500}
         />
       </div>
     </>
